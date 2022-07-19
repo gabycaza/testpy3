dc74f45d976c46dbc2fc69ea885f08eb746380a7
gabycaza <109319001+gabycaza@users.noreply.github.com>
2022-07-19 19:23:49 -0300

    add file

---
+++ contador de palabras.py
@@ -0,0 +1,17 @@
+
+frase1 = input("ingrese una frase 1    ")
+frase2 = input("ingrese una frase 2    ")
+frase3 = input("ingrese una frase 3    ")
+
+lista1 = [frase1, frase2, frase3]
+
+
+while [lista1]!= "abra":
+    frase1 = input("ingrese una frase 1    ")
+    frase2 = input("ingrese una frase 2    ")
+    frase3 = input("ingrese una frase 3    ")   
+else:
+    print ("termino el ejercicio")
+    
+print (len (lista1))
+print (len (lista1), (frase1))
