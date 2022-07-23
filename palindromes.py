591b8e0786a42537654dc21c1b7ca192b2e81d0c
jairo quispe <jairo.quispe@openix.com.ar>
2022-07-23 20:30:04 -0300

    - Se agrega README.md - Se agrega comentarios para correcciones o mejoras en el codigo - Se agrega .gitignore

--- palindromes.py
+++ palindromes.py
@@ -2,13 +2,15 @@ palabras_ingresadas =0
 palindromos = 0
 no_palindromos= 0
 palabra = 0
+
+# El while esta contando una palabra de mas, ya que la ultima palabra no deberia contarse
 while palabra!= "aloha" and palabra !="bye":
-    palabra= input("Ingrese una palabra: ")    
+    palabra= input("Ingrese una palabra: ")
     if(palabra) ==(palabra)[::-1]:
         print("es palindromo")
         palindromos +=1
         palabras_ingresadas +=1
-    else: 
+    else:
         print("No es palindromo")
         palabras_ingresadas +=1
         no_palindromos +=1
