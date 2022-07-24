83031c4eac1791feb23eb2d2d0cc3a7f2c8e7065
gabycaza <109319001+gabycaza@users.noreply.github.com>
2022-07-24 18:22:14 -0300

    ahi agregue el nuevo

--- word_counter.py
+++ word_counter.py
@@ -1,1 +1,25 @@
+contador_de_frases = 0
+frase = input("ingrese una frase     ")
+palabras_totales = 0
 
+cantidad_de_palabras = frase.count (' ')+1
+contador_de_frases = contador_de_frases +1
+print (f"la frase tiene   ", cantidad_de_palabras, "palabras")
+print (f"la cantidad de frase es   ", contador_de_frases)
+
+f = open ("frases.txt",'a')
+f.write(frase+'\n')
+
+while frase != "the end":
+    frase = input ("ingrese una frase:    ")
+    
+    cantidad_de_palabras = frase.count (' ')+1
+    contador_de_frases = ( contador_de_frases)+1
+    
+    print (f"la cantidad de palabras son   ", cantidad_de_palabras, "palabras")
+    print (f"la cantidad de frases total es  " , contador_de_frases)
+
+palabras_total = (palabras_totales+cantidad_de_palabras )
+
+print (f"la cantidad de palabras totales son  ", cantidad_de_palabras , "palabras")
+print (f"la cantidad de frases totales son   ",  contador_de_frases, "frases")
