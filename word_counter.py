85a7933b5e801b16369d59648f4deaa97ac2c1f4
gabycaza <109319001+gabycaza@users.noreply.github.com>
2022-07-24 18:21:24 -0300

    Update word_counter.py
    Signed-off-by: gabycaza <109319001+gabycaza@users.noreply.github.com>

--- word_counter.py
+++ word_counter.py
@@ -1,15 +1,1 @@
 
-frases = input("ingrese una frase     ")
-
-while frases != "the end":
-
-    f = open ("frases.txt",'a')
-    f.write(frases+'\n')
-    frases = input ("ingrese una frase:    ")
-
-cantidad_de_frases= len (frases)
-cantidad_de_palabras =  (frases.split())
-
-
-print ("la cantidad de frases es   " ,cantidad_de_frases)
-print ("la cantidad de palabras es   ", len (cantidad_de_palabras))
