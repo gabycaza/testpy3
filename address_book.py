f7298ab5b0cdc286c454ad85eb7bd7515155cb33
DigiWik <109569229+DigiWik@users.noreply.github.com>
2022-07-28 00:41:35 -0300

    pequeños ajustes
    voy a modificar la fecha para que quede como leemos en nuestro pais, dia/mes/año y luego modificar la ubicacion del from csv import writer q era lo que estaba fallando antes, ahora con el cambio se ejecuta el archivo csv
    
    Signed-off-by: DigiWik <109569229+DigiWik@users.noreply.github.com>

--- address_book.py
+++ address_book.py
@@ -1,6 +1,10 @@
-nombre_apellido = input ("ingrese su nombre y apellido ")
+import random
+import datetime
+from csv import writer
+
+nombre_apellido = str(input ("ingrese su nombre y apellido "))
 direccion= input ("ingrese su dirección ")
-telefono = input ("ingrese su número de teléfono ")
+telefono = int(input ("ingrese su número de teléfono "))
 
 from random import random
 id = []
@@ -9,24 +13,12 @@ for i in range(1):
 print(f"el ID de {nombre_apellido} es {id}")
 
 from datetime import date
-fecha_registro = date.today()
+fecha_registro = date.today().strftime('%d/%m/%y')
 
 print("La fecha de registro es", fecha_registro)
 
 libreta_direcciones = [nombre_apellido, direccion, telefono, id, fecha_registro]
 
-import csv
-
-with open('adresses.csv', 'w') as csvfile:
-    fieldnames = [nombre_apellido, direccion, telefono, id, fecha_registro]
-    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-
-    writer.writeheader
-    writer.writerow({'nombre_apellido',
-                     'direccion',
-                     'telefono',
-                     'id',
-                     'fecha_registro'})
-
-print ("datos insertados")
-
+with open('addresses.csv', 'a', newline='') as registro:
+    csv_writer = writer(registro)
+    csv_writer.writerow(libreta_direcciones)
